28981a07f9eab549cfa588ee1d53791135d4c251
Vishal U J <vishaluj1203@gmail.com>
2025-06-17 02:28:35 +0000

    chore: Add docs for the method

--- python/001-two-sum.py
+++ python/001-two-sum.py
@@ -1,9 +1,18 @@
 from typing import List
 
-
 class Solution:
-    # Brute Force
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force
+        """
+        [Two Sum Problem](https://leetcode.com/problems/two-sum/)
+
+        Args:
+            nums (List[int]): List of integers.
+            target (int): Integer.
+
+        Returns:
+            List[int]: List of intgers which sum up to target.
+        """
         for i in range(0, (len(nums))):
             for j in range(i + 1, (len(nums))):
                 if nums[i] + nums[j] == target:
