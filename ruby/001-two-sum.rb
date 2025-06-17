4074a0397cfbb2636548fa26d432194d0c7a976d
Vishal U J <vishaluj1203@gmail.com>
2025-06-17 02:28:17 +0000

    refactor(two-sum): Remove constraints and redundant variable

--- ruby/001-two-sum.rb
+++ ruby/001-two-sum.rb
@@ -2,13 +2,9 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    nums_range = nums.length
-    return [] if nums_range > 10**4 || nums_range < 2  
     nums.each_with_index do |num, index|
-        ((index + 1)...nums_range).each do |ix|
-            if (num + nums[ix] == target)
-                return [index, ix]
-            end
+        ((index + 1)...nums.length).each do |ix|
+            return [index, ix] if (num + nums[ix] == target)
         end
     end
     return []
